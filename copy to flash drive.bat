da08a3a61aafb1ecbe6f16b0c60bd36402071ea1
Giantvince1 <giantvince1@protonmail.com>
2024-10-07 12:16:57 -0500

    Nuke the repo and start fresh, keeping license and readme files.
    Signed-off-by: Giantvince1 <giantvince1@protonmail.com>

--- copy to flash drive.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo This script is about to copy all data from the AppData folder to the flash drive.
-echo It will kill the Minecraft-related contents of the flash drive before it does so.
-echo If you wish to proceed, press any key. Otherwise press CTRL+C or click the X to abort.
-pause > NUL
-rmdir /S /Q D:\.minecraft
-rmdir /S /Q D:\.tlauncher
-robocopy %APPDATA%\.minecraft D:\.minecraft /E /COPYALL /MT:2 /DCOPY:DATE
-robocopy %APPDATA%\.tlauncher D:\.tlauncher /E /COPYALL /MT:2 /DCOPY:DATE
--- optimize windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-D:
-cd \Scripts\
-for /F "delims=" %%I in ( services.txt ) do (
-	sc stop "%%~I"
-	sc config "%%~I" start=disabled
-)
